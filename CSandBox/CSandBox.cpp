9985f469c6cdb4bb30c746d73010f8ed61da1731
khaled elsayed <59780800+khaled71612000@users.noreply.github.com>
2023-06-30 00:24:43 +0300

    List

--- CSandBox/CSandBox.cpp
+++ CSandBox/CSandBox.cpp
@@ -1,44 +1,26 @@
 // CSandBox.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include <iostream>
-#include <vector>
+#include <list>
 
 using namespace std;
 
 int main()
 {
-    int a[2];
-    a[0] = 0;
-    a[1] = 1;
 
-    for (int i = 0; i < 2; i++)
+    list<char> v;
+
+    for (int i = 0; i < 10; i++)
     {
-        cout << a[i] << endl;
+        v.push_back('A');
     }
-    cout << endl;
+    cout << v.size() << endl;
 
-    vector<int> v(2);
-    v[0] = 0;
-    v[1] = 1;
-
-    for (int i = 0; i < v.size(); i++)
-    {
-        cout << v[i] << endl;
-    }
-    cout << endl;
-
-    v.push_back(3);
-
-    for (const int& i : v)
+    for (const char& i : v)
     {
         cout << i << endl;
     }
     cout << endl;
 
-    v.resize(1);
-    for (const int& i : v)
-    {
-        cout << i << endl;
-    }
     std::cin.get();
 }
