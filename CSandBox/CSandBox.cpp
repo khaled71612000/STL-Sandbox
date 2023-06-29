6da4e9b13c3c664217f2e230d9b083f19c290b61
khaled elsayed <59780800+khaled71612000@users.noreply.github.com>
2023-06-29 20:12:55 +0300

    Vectors

--- CSandBox/CSandBox.cpp
+++ CSandBox/CSandBox.cpp
@@ -1,27 +1,44 @@
 // CSandBox.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
-#include <thread>
+#include <vector>
 
-static bool s_finished = false;
-
-void DoWork()
-{
-    while (!s_finished)
-    {
-        std::cout << "hello..\n";
-    }
-}
+using namespace std;
 
 int main()
 {
-    std::thread worker(DoWork);
+    int a[2];
+    a[0] = 0;
+    a[1] = 1;
 
-    std::cin.get();
-    s_finished = true;
+    for (int i = 0; i < 2; i++)
+    {
+        cout << a[i] << endl;
+    }
+    cout << endl;
 
-    worker.join();
+    vector<int> v(2);
+    v[0] = 0;
+    v[1] = 1;
 
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << endl;
+    }
+    cout << endl;
+
+    v.push_back(3);
+
+    for (const int& i : v)
+    {
+        cout << i << endl;
+    }
+    cout << endl;
+
+    v.resize(1);
+    for (const int& i : v)
+    {
+        cout << i << endl;
+    }
     std::cin.get();
 }
